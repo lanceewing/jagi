bd9b9ab67c158649b8cca79c20a217d0c0063337
Lance Ewing <lance.ewing@gmail.com>
2016-10-29 20:56:04 +0100

    Adding three unknown Instruction classes for use during AGI v1 LOGIC script investigations.

---
+++ src/com/sierra/agi/logic/interpret/instruction/InstructionUnknown1.java
@@ -0,0 +1,35 @@
+package com.sierra.agi.logic.interpret.instruction;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import com.sierra.agi.logic.Logic;
+import com.sierra.agi.logic.LogicContext;
+import com.sierra.agi.logic.LogicException;
+import com.sierra.agi.logic.interpret.LogicReader;
+
+public class InstructionUnknown1 extends InstructionUni {
+    /** 
+     * Creates new Unknown instruction with one parameter.
+     *
+     * @param context   Game context where this instance of the instruction will be used. (ignored)
+     * @param stream    Logic Stream. Instruction must be written in uninterpreted format.
+     * @param reader    LogicReader used in the reading of this instruction. (ignored)
+     * @param bytecode  Bytecode of the current instruction.
+     * @throws IOException I/O Exception are throw when <CODE>stream.read()</CODE> fails.
+     */
+    public InstructionUnknown1(InputStream stream, LogicReader reader, short bytecode, short engineEmulation) throws IOException
+    {
+        super(stream, bytecode);
+    }
+
+    @Override
+    public int execute(Logic logic, LogicContext logicContext) throws LogicException, Exception {
+        return 2;
+    }
+
+    @Override
+    public String[] getNames() {
+        return new String[] {"unknown", Integer.toString(p1)};
+    }
+}
---
+++ src/com/sierra/agi/logic/interpret/instruction/InstructionUnknown2.java
@@ -0,0 +1,40 @@
+package com.sierra.agi.logic.interpret.instruction;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import com.sierra.agi.logic.Logic;
+import com.sierra.agi.logic.LogicContext;
+import com.sierra.agi.logic.LogicException;
+import com.sierra.agi.logic.interpret.LogicReader;
+
+public class InstructionUnknown2 extends InstructionBi {
+
+    /** 
+     * Creates new Unknown instruction with two parametes.
+     *
+     * @param context   Game context where this instance of the instruction will be used. (ignored)
+     * @param stream    Logic Stream. Instruction must be written in uninterpreted format.
+     * @param reader    LogicReader used in the reading of this instruction. (ignored)
+     * @param bytecode  Bytecode of the current instruction.
+     */
+    public InstructionUnknown2(InputStream stream, LogicReader reader, short bytecode, short engineEmulation) throws IOException {
+        super(stream, bytecode);
+    }
+
+    @Override
+    public int execute(Logic logic, LogicContext logicContext) throws LogicException, Exception {
+        return 3;
+    }
+
+    @Override
+    public String[] getNames() {
+        String[] names = new String[3];
+        
+        names[0] = "unknown" + Integer.toHexString(bytecode);
+        names[1] = Integer.toString(p1);
+        names[2] = Integer.toString(p2);
+
+        return names;
+    }
+}
