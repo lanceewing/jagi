cc1cf048f2a05795f7cd2f6b1d5b6a5975ce8026
Lance Ewing <lance.ewing@gmail.com>
2016-10-29 20:53:39 +0100

    Adding the three new Said test expression classes to handle AGI v1 'said' commands.

---
+++ src/com/sierra/agi/logic/interpret/expression/ExpressionSaidTwoWords.java
@@ -0,0 +1,17 @@
+package com.sierra.agi.logic.interpret.expression;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import com.sierra.agi.logic.interpret.LogicReader;
+
+public class ExpressionSaidTwoWords extends ExpressionSaid {
+
+    public ExpressionSaidTwoWords(InputStream stream, LogicReader reader, short bytecode, short engineEmulation) throws Exception {
+        super(stream, reader, bytecode, engineEmulation);
+    }
+
+    protected int getWordCount(InputStream stream) throws IOException {
+        return 2;
+    }
+}
